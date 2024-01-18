cdf7f172c2bf0ed59d6cb4645e4e0774f8bebeeb
khd <khd>
2024-01-18 15:57:16 +0900

    완강

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -2,6 +2,11 @@ package study.querydsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +15,8 @@ public class QuerydslApplication {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
 }
